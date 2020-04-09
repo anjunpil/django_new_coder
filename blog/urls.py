93c2d79da2b8d3a3a200447b539407f670754fbf
anjunpil <hhjs6272@naver.com>
2020-04-09 15:11:26 +0900

    help

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('',include('blog.urls')),
+	path('',views.post_list,name = 'post_list'),
 ]
