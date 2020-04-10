6be01d816b8d3d0463d19bb251661af5518ecbaa
anjunpil <hhjs6272@naver.com>
2020-04-10 15:32:27 +0900

    django_tutorial

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static') #정적파일 경로추가
+STATIC_ROOT = os.path.join(BASE_DIR,'static'), #정적파일 경로추가
